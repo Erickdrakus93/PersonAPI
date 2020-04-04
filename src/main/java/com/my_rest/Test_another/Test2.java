e964c869c18e36645df907ad9de887f8b29905bf
Erick Hdz <erikh93@ciencias.unam.mx>
2020-04-03 22:08:49 -0600

    Adding

--- src/main/java/com/my_rest/Test_another/Test2.java
+++ src/main/java/com/my_rest/Test_another/Test2.java
@@ -1,9 +1,11 @@
 package com.my_rest.Test_another;
 
+import java.awt.*;
+
 public class Test2 {
     public static void main(String[] args) {
-        for (int i= 1;i<100;i++){
-            System.out.println("i:" +  i*10);
+        for (int i=1000; i>0;i--){
+            System.out.println("i" +  i* 10);
             System.out.println("\n");
         }
     }
